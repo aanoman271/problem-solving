0340c3206a43ef8a079664d77de6f8380aab5feb
noman291 <aanoman291@gmail.com>
2026-01-25 03:35:26 +0600

    vowel Count

---
+++ vowelCount.js
@@ -0,0 +1,8 @@
+const vowelCount = (string) => {
+  const vowels = "aeiou";
+  const filtaredVowels = [...string.toLowerCase()].filter((v) =>
+    vowels.includes(v),
+  );
+  return filtaredVowels.length;
+};
+console.log(vowelCount("dhusaidlhi"));
